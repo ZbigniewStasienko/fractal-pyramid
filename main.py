0268d0683e82357e95b798aa30eea2e90d6a9519
Zbyszek Stasienko <zbyszekstasienko@gmail.com>
2023-11-13 13:53:03 +0100

    simple rotating cube

--- main.py
+++ main.py
@@ -1,5 +1,69 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+#Learning PyOpenGl
+#Creating sample cube
 
+import pygame
+from pygame.locals import *
 
-print_hi('PyCharm')
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+#definig vertices
+vertices = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1),
+)
+
+#definig edges
+edges = (
+    (0,1),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7),
+)
+
+#definig Cube object
+def Cube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+def main():
+    pygame.init()
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+
+    glTranslatef(0.0, 0.0, -5)
+
+    glRotatef(0, 0, 0, 0)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(0.5, 0, 1, 0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+main()

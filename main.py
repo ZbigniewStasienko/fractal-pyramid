af5e72953729cd347776a4992fc83a2c22610294
Zbyszek Stasienko <zbyszekstasienko@gmail.com>
2023-11-19 23:32:34 +0100

    rotating pyramid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-#Learning PyOpenGl
-#Adding key events
+from math import sqrt
 
 import pygame
 from pygame.locals import *
@@ -7,65 +6,24 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-#definig vertices
 vertices = (
-    (1, -1, -1),
-    (1, 1, -1),
-    (-1, 1, -1),
-    (-1, -1, -1),
-    (1, -1, 1),
-    (1, 1, 1),
-    (-1, -1, 1),
-    (-1, 1, 1)
+    (-1, 0, sqrt(3)/3),
+    (1, 0, sqrt(3)/3),
+    (0, 0, -sqrt(3)*2/3),
+    (0, sqrt(15)/3, 0)
 )
 
-#definig edges
 edges = (
     (0,1),
+    (0,2),
     (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-)
-
-#definig surfaces
-surfaces = (
-    (0, 1, 2, 3),
-    (3, 2, 7, 6),
-    (6, 7, 5, 4),
-    (4, 5, 1, 0),
-    (1, 5, 7, 2),
-    (4, 0, 3, 6)
-)
-
-colors = (
-    (0, 1, 0),
-    (0, 1, 1),
-    (1, 0, 0),
-    (1, 0, 1),
-    (1, 1, 0),
-    (1, 1, 1)
+    (1,2),
+    (1,3),
+    (2,3)
 )
 
 #definig Cube object
-def Cube():
-
-    glBegin(GL_QUADS)
-    x = 0
-    for surface in surfaces:
-        glColor3fv(colors[x])
-        x += 1
-        for vertex in surface:
-            glVertex3fv(vertices[vertex])
-    glEnd()
-
+def Triangle():
 
     glBegin(GL_LINES)
     for edge in edges:
@@ -75,12 +33,12 @@ def Cube():
 
 def main():
     pygame.init()
-    display = (800,600)
+    display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
-    glTranslatef(0.0, 0.0, -10)
+    glTranslatef(0.0, 0.0, -5)
 
     while True:
         for event in pygame.event.get():
@@ -88,22 +46,9 @@ def main():
                 pygame.quit()
                 quit()
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    glTranslatef(-1,0,0)
-                if event.key == pygame.K_RIGHT:
-                    glTranslatef(1, 0, 0)
-                if event.key == pygame.K_UP:
-                    glTranslatef(0,1,0)
-                if event.key == pygame.K_DOWN:
-                    glTranslatef(0, -1, 0)
-
-
-
-        #glRotatef(0.5, 0, 1, 0)
+        glRotatef(0.5, 0, 1, 0)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        Cube()
+        Triangle()
         pygame.display.flip()
         pygame.time.wait(10)
-
 main()
